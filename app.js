28cb5ad98035b2ee5758f333b054475cf86c3aa5
Jenei David <jenei.david1994@gmail.com>
2020-07-04 10:10:44 +0200

    create publish new post and basic designs and functionalities

--- app.js
+++ app.js
@@ -15,16 +15,37 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts = [];
+
 app.get("/", function(req, res){
-	res.render("home");
-})
+	res.render("home", {
+		content: homeStartingContent, 
+		posts: posts
+	});	
+});
 
+app.get("/contact", function(req, res){
+	res.render("contact", {content: aboutContent});
+});
 
+app.get("/about", function(req, res){
+	res.render("about", {content: contactContent});
+});
 
+app.get("/compose", function(req, res){
+	res.render("compose", {content: contactContent});
+});
 
+app.post("/compose", function(req, res){
 
+	let post = {
+		title: req.body.postTitle,
+		text: req.body.postText
+	};
 
-
+	posts.push(post);
+	res.redirect("/");
+});
 
 
 
